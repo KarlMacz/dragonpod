212ce270f85b08ca5bd8a81e0bd94739a15c8821
Karl T. Macadangdang <karljarren0308@gmail.com>
2019-10-17 23:32:33 +0800

    Removed Slide Alert and Interrupt

--- employee/layouts/footer.php
+++ employee/layouts/footer.php
@@ -1,15 +1,2 @@
-  <div class="slide-alert-group"></div>
-  <div class="interrupt">
-    <div class="interrupt-content">
-      <div class="interrupt-content-inner">
-        <div class="text-center">
-          <div class="mb-2">
-            <span class="fas fa-cog fa-4x fa-spin fa-fw"></span>
-          </div>
-          <h3>Processing request...</h3>
-        </div>
-      </div>
-    </div>
-  </div>
 </body>
 </html>
